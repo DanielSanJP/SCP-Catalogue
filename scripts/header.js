e560b302377079cd6cf5804cad307b170bfff0c0
DanielSanJP <105352895+DanielSanJP@users.noreply.github.com>
2024-08-20 11:14:16 +1200

    Initial Commit

---
+++ scripts/header.js
@@ -0,0 +1,23 @@
+// header.js
+export const headerTemplate = `
+    <header>
+        <a href="index.html" class="logo">
+           <img src="/images/SCP LOGO.png" alt="SCP Foundation Logo" height="50">
+        </a>
+        <nav>
+            <ul class="nav-links">
+                <li><a href="index.html">Home</a></li>
+                <li><a href="catalogue.html">SCP Catalog</a></li>
+                <li><a href="disruptionclass.html">Disruption Classes</a></li>
+                <li><a href="objectclass.html">Containment Classes</a></li>
+                <li><a href="riskclass.html">Risk Classes</a></li>
+            </ul>
+        </nav>
+                    <div class="burger">
+                <div class="line1"></div>
+                <div class="line2"></div>
+                <div class="line3"></div>
+            </div>
+    </header>
+`;
+
