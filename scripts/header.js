948d8c4362ea834805dcd27c331494ff330075c1
DanielSanJP <105352895+DanielSanJP@users.noreply.github.com>
2024-08-20 11:34:22 +1200

    Fix some syntax for cPanel to work.

--- scripts/header.js
+++ scripts/header.js
@@ -2,7 +2,7 @@
 export const headerTemplate = `
     <header>
         <a href="index.html" class="logo">
-           <img src="/images/SCP LOGO.png" alt="SCP Foundation Logo" height="50">
+           <img src="./images/SCP LOGO.png" alt="SCP Foundation Logo" height="50">
         </a>
         <nav>
             <ul class="nav-links">
